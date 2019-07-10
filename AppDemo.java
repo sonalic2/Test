f6f9df18479c9ea75d2b3cefa5afdfb76e33a65f
sonali <sonalic2@kpit.com>
2019-07-10 13:10:11 +0530

    first version of appdemo

---
+++ AppDemo.java
@@ -0,0 +1,13 @@
+package sample.sample;
+
+/**
+ * Hello world!
+ *
+ */
+public class AppDemo 
+{
+    public void getDemo( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
